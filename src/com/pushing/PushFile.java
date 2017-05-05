576c9d3eeebbc98da07b5d92b8b16e8f6a1b8afe
anujgautam <anuj@acadgild.com>
2017-05-05 20:05:29 +0530

    First commit

---
+++ src/com/pushing/PushFile.java
@@ -0,0 +1,11 @@
+package com.pushing;
+
+public class PushFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Please run this time");
+	}
+
+}
